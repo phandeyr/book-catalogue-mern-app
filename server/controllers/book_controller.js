736de901683cc7b149dee8a7c969d0781a4e572e
phandeyr <phandeyr@gmail.com>
2020-07-05 19:08:36 +0100

    implemented additional book routes

--- server/controllers/book_controller.js
+++ server/controllers/book_controller.js
@@ -11,6 +11,60 @@ exports.createBook = async (req, res) => {
       res.status(201).json(result)
     }
   } catch (err) {
-    res.status(500).send('Unable to create book')
+    res.status(500).json({ message: 'Unable to create book' })
+  }
+}
+
+/**
+ * Gets all books
+ */
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find()
+    if (books) {
+      res.json(books)
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Unable to retrieve all books' })
+  }
+}
+
+/**
+ * Gets a specific book's detail
+ */
+exports.getBookDetail = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+    if (book) {
+      res.json(book)
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Unable to retrieve book' })
+  }
+}
+
+/**
+ * Updates a specific book
+ */
+exports.updateBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (book) {
+      res.json(book)
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Unable to update book' })
+  }
+}
+
+/**
+ * Deletes a specific book
+ */
+exports.deleteBook = async (req, res) => {
+  try {
+    await Book.deleteOne({ _id: req.params.id })
+    res.status(204).send()
+  } catch (err) {
+    res.status(500).json({ message: 'Unable to delete book' })
   }
 }
--- server/routes/book_route.js
+++ server/routes/book_route.js
@@ -8,4 +8,16 @@ const router = express.Router()
 // Post - creates a book
 router.post('/', authController.authenticateToken, authController.authoriseUserRole(roles.ADMIN_ROLE), bookController.createBook)
 
+// Get - gets all books
+router.get('/', authController.authenticateToken, bookController.getBooks)
+
+// Get - gets a specific book detail
+router.get('/:id', authController.authenticateToken, bookController.getBookDetail)
+
+// Post - updates a specific book
+router.post('/:id', authController.authenticateToken, authController.authoriseUserRole(roles.ADMIN_ROLE), bookController.updateBook)
+
+// Delete - deletes a specific book
+router.delete('/:id', authController.authenticateToken, authController.authoriseUserRole(roles.ADMIN_ROLE), bookController.deleteBook)
+
 module.exports = router
