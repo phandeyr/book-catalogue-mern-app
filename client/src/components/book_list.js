41cb245e54b4c3c10e3b078bf45cb92416bc795f
phandeyr <phandeyr@gmail.com>
2020-07-20 21:03:29 +0100

    used react bootstrap table pagination to display books

--- client/src/components/book_list.js
+++ client/src/components/book_list.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
+import LocalStorage from '../utils/local_storage'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import BootstrapTable from 'react-bootstrap-table-next'
 
-const BookList = () => {
-  return (
-    <h3>Books</h3>
-  )
+class BookList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks()
+  }
+
+  getBooks() {
+    const headers = { 
+      'Content-Type': 'application/json',
+      'Authorization': `BEARER ${LocalStorage.getAccessToken()}`
+    }
+
+    fetch('/books', { method: 'GET', headers: headers })
+    .then((res) => {
+      if (res.status === 403) {
+
+      }
+      return res.json()
+    })
+    .then((data) => {
+      this.setState({
+        books: data,
+        isLoading: false
+      })
+    })
+    .catch(console.log)
+  }
+
+  getColumns() {
+    return [{
+      dataField: 'title',
+      text: 'Title'
+    }, {
+      dataField: 'description',
+      text: 'Description'
+    }]
+  }
+
+  getOptions() {
+    return { showTotal: true }
+  }
+
+  render() {
+    console.log('in booklist')
+
+    if (this.state.isLoading) {
+      return 'loading'
+    }
+
+    return (
+      <BootstrapTable
+        keyField='_id' 
+        data={ this.state.books } 
+        columns={ this.getColumns() } 
+        striped
+        hover
+        noDataIndication='There are currently no books'
+        pagination={ paginationFactory(this.getOptions()) } />
+    )
+  }
 }
 
 export default BookList
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './components/app'
 import './css/stylesheet.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css'
+import '../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
