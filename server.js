2014e281690ce14168cd2809756253607df5eb99
phandeyr <phandeyr@gmail.com>
2020-08-12 10:06:53 +0100

    moved catchall logic if production env

--- server.js
+++ server.js
@@ -27,11 +27,10 @@ app.use('/books', book)
 app.use('/auth', auth)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.use(express.static('client/build'))
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
-
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
 
 app.listen(port, () => { console.log('server up and running') })
